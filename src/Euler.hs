182bd9067435e18d41ca02019524e9e738d7dbca
Yakov Pechersky <pechersk@deshawresearch.com>
2017-12-27 18:12:19 -0500

    naive 003 soln

--- src/Euler.hs
+++ src/Euler.hs
@@ -1,8 +1,10 @@
 module Euler
   ( module Euler.Problem001
   , module Euler.Problem002
+  , module Euler.Problem003
   )
   where
 
 import Euler.Problem001
 import Euler.Problem002
+import Euler.Problem003
---
+++ src/Euler/Problem003.hs
@@ -0,0 +1,18 @@
+module Euler.Problem003
+  ( prob003
+  )
+  where
+
+import Data.Numbers.Primes (primeFactors)
+
+{- The prime factors of 13195 are 5, 7, 13 and 29.
+ -
+ - What is the largest prime factor of the number 600851475143 ? -}
+
+prob003 :: Integer
+prob003 = prob003' 600851475143
+
+-- naive method
+
+prob003' :: Integral int => int -> int
+prob003' = maximum . primeFactors
