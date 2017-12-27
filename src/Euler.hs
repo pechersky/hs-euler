ea8efac14545bda58749f985dcadf1c26ac5fc03
Yakov Pechersky <pechersk@deshawresearch.com>
2017-12-27 18:30:57 -0500

    naive 004 soln

--- src/Euler.hs
+++ src/Euler.hs
@@ -2,9 +2,11 @@ module Euler
   ( module Euler.Problem001
   , module Euler.Problem002
   , module Euler.Problem003
+  , module Euler.Problem004
   )
   where
 
 import Euler.Problem001
 import Euler.Problem002
 import Euler.Problem003
+import Euler.Problem004
---
+++ src/Euler/Problem004.hs
@@ -0,0 +1,25 @@
+module Euler.Problem004
+  ( prob004
+  )
+  where
+
+{- A palindromic number reads the same both ways.
+ - The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ -
+ - Find the largest palindrome made from the product of two 3-digit numbers. -}
+
+prob004 :: Integer
+prob004 = prob004' $ 1e3 - 1
+
+-- naive method
+
+palindrome :: Integer -> Bool
+palindrome = palindromeStr . show
+  where
+    palindromeStr :: String -> Bool
+    palindromeStr str = and $ zipWith (==) str (reverse str)
+
+prob004' :: Integer -> Integer
+prob004' bound = maximum . filter palindrome $ [x * y | x <- possible, y <- possible, x <= y]
+  where
+    possible = [bound, bound-1 .. 1]
