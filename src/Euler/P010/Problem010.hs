d4adf02330ef255143e738150102dabcbdf2cb87
Yakov Pechersky <pechersk@deshawresearch.com>
2017-12-29 15:19:39 -0500

    prob010 soln

--- src/Euler/P010/Problem010.hs
+++ src/Euler/P010/Problem010.hs
@@ -3,12 +3,16 @@ module Euler.P010.Problem010
   )
   where
 
-{- Problem statement here -}
+{- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ -
+ - Find the sum of all the primes below two million. -}
+
+import Data.Numbers.Primes (primes)
 
 prob010 :: Integer
-prob010 = prob010' undefined
+prob010 = prob010' 2e6
 
 -- naive method
 
 prob010' :: Integer -> Integer
-prob010' = undefined
+prob010' limit = sum . takeWhile (< limit) $ primes
