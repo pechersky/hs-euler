4b900fd339fff6258e2f2dc7de3cb0eb4fc8eef5
Yakov Pechersky <pechersk@deshawresearch.com>
2017-12-29 15:15:45 -0500

    change template file source

--- makedir.sh
+++ makedir.sh
@@ -11,7 +11,7 @@ for i in $(seq 0 9); do
   target=$(printf "%03d" $(($i + $1)))
   itarget=$(printf "%03d" $(($i)))
   ftarget="$fdir/Problem$target.hs"
-  sed "s/prob$fsource/prob$target/g" "src/Euler/Problem$fsource.hs" > $ftarget
+  sed "s/prob$fsource/prob$target/g" "template/Problem$fsource.hs" > $ftarget
   sed -i "s/Problem$fsource/Problem$target/g" $ftarget
   sed -i "s/Problem$itarget/Problem$target/g" "$fdir/P$fbase.hs"
   sed -i "s/P$fsource/P$fbase/g" $ftarget
