4ede783184151505f3668938d513aa6f708f3299
Yakov Pechersky <pechersk@deshawresearch.com>
2017-12-29 14:26:27 -0500

    make accessing problem solns work with TH

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
+
+import Text.Printf (printf)
+import Language.Haskell.TH
 
 import Euler()
 
 main :: IO ()
 main = print "helloworld"
+
+prob :: Integer -> Q Exp
+prob num = varE (mkName ("prob" ++ printf "%03d" num))
+
