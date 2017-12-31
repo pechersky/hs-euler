d860ae1c9cd4afd96b80171996d34466eeb737d5
Yakov Pechersky <ffxen158@gmail.com>
2017-12-31 13:28:04 -0500

    prob016 soln

--- src/Euler/P010/Problem016.hs
+++ src/Euler/P010/Problem016.hs
@@ -3,12 +3,16 @@ module Euler.P010.Problem016
   )
   where
 
-{- Problem statement here -}
+{-
+ -2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ -
+ -What is the sum of the digits of the number 2^1000?
+ -}
 
 prob016 :: Integer
-prob016 = prob016' undefined
+prob016 = prob016' 1000
 
 -- naive method
 
 prob016' :: Integer -> Integer
-prob016' = undefined
+prob016' = sum . fmap (read . replicate 1) . show @Integer . (2 ^)
