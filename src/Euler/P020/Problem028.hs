079a7565739fc9012d48dba7c2c1260126995bf6
Yakov Pechersky <ffxen158@gmail.com>
2018-01-01 22:01:44 -0500

    prob028 soln

--- src/Euler/P020/Problem028.hs
+++ src/Euler/P020/Problem028.hs
@@ -3,12 +3,29 @@ module Euler.P020.Problem028
   )
   where
 
-{- Problem statement here -}
+{-
+ -Starting with the number 1 and moving to the right in a
+ -clockwise direction a 5 by 5 spiral is formed as follows:
+ -
+ -21 22 23 24 25
+ -20  7  8  9 10
+ -19  6  1  2 11
+ -18  5  4  3 12
+ -17 16 15 14 13
+ -
+ -It can be verified that the sum of the numbers on the diagonals is 101.
+ -
+ -What is the sum of the numbers on the diagonals
+ -in a 1001 by 1001 spiral formed in the same way?
+ -}
 
 prob028 :: Integer
-prob028 = prob028' undefined
+prob028 = prob028' 1001
 
 -- naive method
 
 prob028' :: Integer -> Integer
-prob028' = undefined
+prob028' limit = sum . fmap (sum . corners) $ [1,3 .. limit]
+  where
+    corners 1 = [1]
+    corners n = take 4 [n * n, n * n - (n - 1) ..]
