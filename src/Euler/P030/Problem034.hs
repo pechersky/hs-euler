4945f1f4974af4eb889ab70f8a8264962a427257
Yakov Pechersky <pechersk@deshawresearch.com>
2018-01-03 13:26:53 -0500

    prob034 soln

--- src/Euler/P030/Problem034.hs
+++ src/Euler/P030/Problem034.hs
@@ -3,12 +3,21 @@ module Euler.P030.Problem034
   )
   where
 
-{- Problem statement here -}
+{- 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+ -
+ - Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+ -
+ - Note: as 1! = 1 and 2! = 2 are not sums they are not included. -}
 
 prob034 :: Integer
-prob034 = prob034' undefined
+prob034 = prob034' (factorial 9)
 
 -- naive method
 
 prob034' :: Integer -> Integer
-prob034' = undefined
+prob034' = sum . filter digitSum . enumFromTo 3
+  where
+    digitSum n = n == (sum . fmap (factorial . read . replicate 1) . show) n
+
+factorial :: Integer -> Integer
+factorial = product . enumFromTo 1
