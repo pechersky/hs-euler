cd3ba403d89955ee0218dbebea81768f87789914
Yakov Pechersky <pechersk@deshawresearch.com>
2018-01-03 16:09:25 -0500

    prob037 soln

--- src/Euler/P030/Problem037.hs
+++ src/Euler/P030/Problem037.hs
@@ -3,12 +3,26 @@ module Euler.P030.Problem037
   )
   where
 
-{- Problem statement here -}
+{- The number 3797 has an interesting property. Being prime itself,
+ - it is possible to continuously remove digits from left to right,
+ - and remain prime at each stage: 3797, 797, 97, and 7.
+ - Similarly we can work from right to left: 3797, 379, 37, and 3.
+ -
+ - Find the sum of the only eleven primes that are both truncatable
+ - from left to right and right to left.
+ -
+ - NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes. -}
+
+import Data.List           (inits, tails)
+import Data.Numbers.Primes (primes, isPrime)
+import Data.Digits         (digits, unDigits)
 
 prob037 :: Integer
-prob037 = prob037' undefined
+prob037 = prob037' 11
 
 -- naive method
 
 prob037' :: Integer -> Integer
-prob037' = undefined
+prob037' (fromIntegral->limit) = sum . take limit . filter truncatable . dropWhile (<= 7) $ primes
+  where
+    truncatable = all (isPrime . unDigits 10) . concat . sequence [init . tails, tail . inits] . digits 10
