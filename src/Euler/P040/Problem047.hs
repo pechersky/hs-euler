7cfd677730d73269364c2a26eba67ba444ff3105
Yakov Pechersky <ffxen158@gmail.com>
2018-01-04 01:40:54 -0500

    prob047 soln

--- src/Euler/P040/Problem047.hs
+++ src/Euler/P040/Problem047.hs
@@ -3,12 +3,29 @@ module Euler.P040.Problem047
   )
   where
 
-{- Problem statement here -}
+{-
+ - The first two consecutive numbers to have two distinct prime factors are:
+ -
+ - 14 = 2 × 7
+ - 15 = 3 × 5
+ -
+ - The first three consecutive numbers to have three distinct prime factors are:
+ -
+ - 644 = 2² × 7 × 23
+ - 645 = 3 × 5 × 43
+ - 646 = 2 × 17 × 19.
+ -
+ - Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
+ -}
+
+import Data.List           (tails, transpose, group)
+import Data.Numbers.Primes (primeFactors)
 
 prob047 :: Integer
-prob047 = prob047' undefined
+prob047 = prob047' 4
 
 -- naive method
 
 prob047' :: Integer -> Integer
-prob047' = undefined
+prob047' (fromIntegral->limit) = head . head . filter (all ((==) limit . length . group . primeFactors))
+                               . transpose . take limit . tails $ [1..]
