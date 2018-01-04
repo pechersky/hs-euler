be21fca61b31d3b051cc89a41d1576df50946df4
Yakov Pechersky <ffxen158@gmail.com>
2018-01-03 20:41:36 -0500

    prob041 soln

--- src/Euler/P040/Problem041.hs
+++ src/Euler/P040/Problem041.hs
@@ -3,12 +3,23 @@ module Euler.P040.Problem041
   )
   where
 
-{- Problem statement here -}
+{-
+ - We shall say that an n-digit number is pandigital if it makes use of all the
+ - digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+ -
+ - What is the largest n-digit pandigital prime that exists?
+ -}
 
-prob041 :: Integer
-prob041 = prob041' undefined
+import Data.List           (sort)
+import Data.Digits         (digits)
+import Data.Numbers.Primes (primes)
+
+{-prob041 :: Integer-}
+prob041 = prob041' 9
 
 -- naive method
 
-prob041' :: Integer -> Integer
-prob041' = undefined
+{-prob041' :: Integer -> Integer-}
+prob041' (fromIntegral->limit) = fromIntegral . last . filter pandigital . takeWhile (< 10 ^ limit) $ primes
+  where
+    pandigital n = let d = digits 10 n in sort d == [1..length d]
