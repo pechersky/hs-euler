10ceff507ed7168ef4e0ca911c470b28bf9ba56c
Yakov Pechersky <ffxen158@gmail.com>
2018-01-04 01:45:49 -0500

    prob048 soln

--- src/Euler/P040/Problem048.hs
+++ src/Euler/P040/Problem048.hs
@@ -3,12 +3,18 @@ module Euler.P040.Problem048
   )
   where
 
-{- Problem statement here -}
+{-
+ - The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+ -
+ - Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+ -}
+
+import Data.Digits (digits, unDigits)
 
 prob048 :: Integer
-prob048 = prob048' undefined
+prob048 = prob048' 1000
 
 -- naive method
 
 prob048' :: Integer -> Integer
-prob048' = undefined
+prob048' = unDigits 10 . reverse . take 10 . reverse . digits 10 . sum . fmap (\x -> x ^ x) . enumFromTo 1
