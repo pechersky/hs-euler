2721af462319a871cdb785e87a90a3960771078b
Yakov Pechersky <ffxen158@gmail.com>
2018-01-03 20:00:22 -0500

    prob040 soln

--- src/Euler/P040/Problem040.hs
+++ src/Euler/P040/Problem040.hs
@@ -3,12 +3,26 @@ module Euler.P040.Problem040
   )
   where
 
-{- Problem statement here -}
+{-
+ -An irrational decimal fraction is created by concatenating the positive integers:
+ -
+ -0.123456789101112131415161718192021...
+ -
+ -It can be seen that the 12th digit of the fractional part is 1.
+ -
+ -If dn represents the nth digit of the fractional part, find the value of the following expression.
+ -
+ -d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+ -}
+
+import Data.Digits (digits)
 
 prob040 :: Integer
-prob040 = prob040' undefined
+prob040 = prob040' 6
 
 -- naive method
 
 prob040' :: Integer -> Integer
-prob040' = undefined
+prob040' = product . fmap ((champer !!) . subtract 1 . (10 ^)) . enumFromTo 0
+  where
+    champer = concatMap (digits 10) [1..]
