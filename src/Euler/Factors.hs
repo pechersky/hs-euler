c53bd6de5a2390cd059969945ffbd7433f76e4a7
Yakov Pechersky <pechersk@deshawresearch.com>
2018-02-01 10:29:52 -0500

    initial attempt at 075

--- src/Euler/Factors.hs
+++ src/Euler/Factors.hs
@@ -6,19 +6,25 @@ module Euler.Factors
   , abundant
   , powerset
   , totient
+  , coprimesTo
+  , go
   )
   where
 
-import           Data.List           (group)
-import qualified Data.IntSet         as IS
-import           Data.IntMap.Strict  (IntMap)
-import qualified Data.IntMap.Strict  as IM
-import           Control.Monad       (filterM)
-import           Control.Arrow       ((&&&))
-import           Data.Numbers.Primes (primeFactors)
+import           Data.List                        (group)
+import           Data.List.Ordered                (nubSort)
+import qualified Data.IntSet                      as IS
+import           Data.IntMap.Strict               (IntMap)
+import qualified Data.IntMap.Strict               as IM
+import           Control.Applicative              (many)
+import           Control.Monad                    (filterM, guard)
+import           Control.Monad.Trans.State.Strict (get, put, evalStateT)
+import           Control.Monad.Trans.Class        (lift)
+import           Control.Arrow                    ((&&&))
+import           Data.Numbers.Primes              (primeFactors, primes)
 
 newtype Factors = Factors {unFactors :: IntMap Int}
-  deriving (Show)
+  deriving (Show, Eq, Ord)
 
 instance Num Factors where
   (+) (Factors f) (Factors g) = Factors $ IM.unionWith max f g
@@ -58,3 +64,29 @@ abundant = (\(x:xs) -> x < sum xs) . divisors
 
 totient :: Factors -> Int
 totient = IM.foldrWithKey (\k v acc -> acc * (k ^ (v - 1)) * (k - 1)) 1 . unFactors
+
+coprimesTo :: Int -> Factors -> [Factors]
+coprimesTo limit n = fmap toF . flip evalStateT (limit, avail) . many $ do
+  (k, ps) <- get
+  {- guard (not (null ps)) -}
+  (p,ps') <- lift $ select ps
+  expon <- lift [1 .. maxpower k p]
+  let k' = k `div` (p ^ expon)
+  guard (k' >= 1)
+  put (k', ps')
+  pure (p, expon)
+    where
+      avail = IS.toAscList $ IS.difference (IS.fromDistinctAscList (takeWhile (<= limit) primes)) (IS.fromList (IM.keys (unFactors n)))
+      maxpower (fromIntegral->a) (fromIntegral->b) = floor @Double (logBase b a)
+      toF = Factors . IM.fromAscList . filter ((/= 0) . snd)
+
+select :: [a] -> [(a, [a])]
+select []     = []
+select (x:xs) = (x,xs) : [(y,x:ys) | (y,ys) <- select xs]
+
+go :: IO String
+go = do
+  pos <- getLine
+  case head pos of
+    'a' -> return pos
+    _   -> go
--- src/Euler/P070/Problem075.hs
+++ src/Euler/P070/Problem075.hs
@@ -5,10 +5,32 @@ module Euler.P070.Problem075
 
 {- Problem statement here -}
 
-prob075 :: Integer
-prob075 = prob075' undefined
+import Data.Maybe            (fromJust, isJust)
+import Control.Monad         (guard)
+import Euler.P000.Problem009 (toInt)
+
+{- prob075 :: Integer -}
+prob075 = prob075' 15000
 
 -- naive method
 
-prob075' :: Integer -> Integer
-prob075' = undefined
+{- prob075' :: Integer -> Integer -}
+prob075' limit = do
+  c <- takeWhile (< (fromIntegral (sq limit))) squares
+  b <- takeWhile (< c) squares
+  let a = c - b
+  guard (b > a)
+  guard (isJust (toInt (sqrt a)))
+  guard (f a + f b + f c <= limit)
+  pure (f a, f b, f c)
+  where
+    sq = (^ (2 :: Integer))
+    f = fromJust . toInt . sqrt
+
+whead [] = []
+whead (x:xs) = [x]
+
+squares = fmap sq [2..]
+  where
+    sq = (^ (2 :: Integer))
+
